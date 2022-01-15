e1a33475cb7976fbc7c3c590ad19fd87a5030c24
adammathewvaughn <adammathewvaughn@gmail.com>
2022-01-14 21:37:24 -0600

    fixed css property-name error

--- scripts.js
+++ scripts.js
@@ -1,49 +1,45 @@
- document.addEventListener("DOMContentLoaded", function () {
-     document.body.style.backgroundColor = "blanchedalmond";
-     let div = document.createElement("div");
-     div.className = "friendsdiv";
-     let para = document.createElement("p");
-     let h3 = document.createElement("h3");
-     let button = document.createElement("button");
-     document.body.appendChild(button);
-     button.innerText = "Sing!";
-     let friendslist = ["Tom", "John", "Bobby", "Tim", "Jimmy",];
-     let lines = "lines";
-     let count = 99;
-     let theSong = `${count} ${lines} of code in my file \n 
-     ${count} ${lines} of cooode.\n 
-     Stand up and shout, "Comment one out!" \n
-     ${count - 1} ${lines} of code in my file.\n`;
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.backgroundColor = "blanchedalmond";
+    let div = document.createElement("div");
+    div.className = "friendsdiv";
+    let para = document.createElement("p");
+    let h3 = document.createElement("h3");
+    let button = document.createElement("button");
+    document.body.appendChild(button);
+    button.innerText = "Sing!";
+    let friendslist = ["Tom", "John", "Bobby", "Tim", "Jimmy",];
+    let lines = "lines";
 
-     
-     for (let count = 99; count > 1; count--) {
-     button.addEventListener("click", function () {
-             for (let i = 0; i < friendslist.length; i++) {
-                 let div = document.createElement("div");
-                 div.className = "songdiv";
-                 document.body.appendChild(div);
-                 document.body.appendChild(para);
-                 document.body.appendChild(h3);
-                 h3.textContent = `${friendslist[i++]}`;
-                 div.appendChild(para);
-                 para.innerText = theSong;
-             };
-         });
-         if (count == 1) {
-             lines = "one more line"; 
-         }
-         let verse2 = "No more lines of code in my file.";
-         if (count == 0){
-             theSong = `${verse2}`;
-         }
-        
-         else {
-             lines = "lines";
-         }
-        
-         }
+    button.addEventListener("click", function () {
+        for (let i = 0; i < friendslist.length; i++) {
+            h3.textContent = friendslist[i];
+            let div = document.createElement("div");
+            div.className = "songdiv";
+            document.body.appendChild(div);
+            document.body.appendChild(para);
+            document.body.appendChild(h3);
+            div.appendChild(para);
+            for (let count = 99; count > 1; count--) {
+                let theSong = `${count} ${lines} of code in my file \n
+                 ${count} ${lines} of cooode.\n
+                 Stand up and shout, "Comment one out!" \n
+                 ${count - 1} ${lines} of code in my file.\n`;
+                para.innerText = theSong;
+                if (count == 1) {
+                    lines = "one more line";
+                }
+                let verse2 = "No more lines of code in my file.";
+                if (count == 0) {
+                    theSong = `${verse2}`;
+                }
 
-     });
+                else {
+                    lines = "lines";
+                }
+            };
+        };
+    });
+});
 
 
 
